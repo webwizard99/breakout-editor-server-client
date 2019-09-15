888bcd9296d69cb13b66b5422c67789ad7453201
Michael Bragg <michael_bragg99@hotmail.com>
2019-09-15 11:52:42 -0600

    Add piping for client side from 'path' module.

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.get('/google1370dc3b6669530c.html', (req, res) => {
   // res.send('../google1370dc3b6669530c.html');
   // res.redirect('./google1370dc3b6669530c.html');
